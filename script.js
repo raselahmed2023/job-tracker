0cf24443661bcd9c13757724adc16f63318bba4a
Rasel Ahmed <raselahmediu22@gmail.com>
2026-02-22 16:49:23 +0600

    new style

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+//count section
+const totalJobs = document.getElementById('totalJobs');
+const totalInterview = document.getElementById('totalInterview');
+const totalRejected = document.getElementById('totalRejected');
 
-//btn toggle
+// btn document
 const allBtn = document.getElementById('allBtn');
 const interviewBtn = document.getElementById('interviewBtn');
 const rejectedBtn = document.getElementById('rejectedBtn');
+
+
+//sections job available
+const jobUpdated = document.getElementById('jobUpdated');
+
+//btn toggle
 
 function resetBtn() {
     allBtn.className = "bg-gray-100  text-gray-700 px-5 py-2";
     interviewBtn.className = "bg-gray-100  text-gray-700 px-5 py-2";
     rejectedBtn.className = "bg-gray-100  text-gray-700 px-5 py-2";
 }
-
-
 allBtn.addEventListener('click', function () {
     resetBtn();
     allBtn.className = "bg-black text-white px-5 py-2";
 })
-
 interviewBtn.addEventListener("click", function () {
     resetBtn();
     interviewBtn.className = "bg-black text-white px-5 py-2";
 })
-
 rejectedBtn.addEventListener("click", function () {
     resetBtn();
     rejectedBtn.className = "bg-black text-white px-5 py-2";
@@ -32,20 +38,19 @@ rejectedBtn.addEventListener("click", function () {
 const cardsSection = document.getElementById('cards');
 const filtered = document.getElementById('filtered');
 
-allBtn.addEventListener('click',function(){
+allBtn.addEventListener('click', function () {
     cardsSection.classList.remove('hidden');
     filtered.classList.add('hidden');
 })
 
-interviewBtn.addEventListener('click',function(){
+interviewBtn.addEventListener('click', function () {
     cardsSection.classList.add('hidden');
     filtered.classList.remove('hidden');
 })
 
-rejectedBtn.addEventListener('click',function(){
+rejectedBtn.addEventListener('click', function () {
     cardsSection.classList.add('hidden');
     filtered.classList.remove('hidden');
 })
 
 
-//btn cards 
