95fa55d93a2cc140926ad99d15756aa32f7d191f
Rasel Ahmed <raselahmediu22@gmail.com>
2026-02-23 15:28:17 +0600

    fix

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ const rejectedBtn = document.getElementById('rejectedBtn');
 const allCards = document.getElementById('cards');
 
 
+// filterSection btn
+const filterSection = document.getElementById('filterSection')
+
+
 // cards interview and rejected
 let interviewList = [];
 let rejectedList = [];
@@ -34,35 +38,136 @@ function calculateCount() {
 calculateCount();
 
 
-//onclick toggle
-
-function toggleStyle(id) {
-    console.log('click');
-}
-
 
 
 //mainContainer
 
 mainContainer.addEventListener('click', function (event) {
-    const parentNode = event.target.parentNode.parentNode;
-    const headTitle=parentNode.querySelector('.headTitle').innerText;
-    const headPara=parentNode.querySelector('.headPara').innerText;
-    const details=parentNode.querySelector('.details').innerText;
-    const status=parentNode.querySelector('.status').innerText;
-    const notes=parentNode.querySelector('.notes').innerText;
+    if (event.target.classList.contains('callInterview')) {
 
-    const cardInfo={
-        headTitle,
-        headPara,
-        details,
-        status,
-        notes
+        const parentNode = event.target.closest('.card');
+
+
+        const headTitle = parentNode.querySelector('.headTitle').innerText;
+        const headPara = parentNode.querySelector('.headPara').innerText;
+        const details = parentNode.querySelector('.details').innerText;
+        const status = parentNode.querySelector('.status').innerText;
+        const notes = parentNode.querySelector('.notes').innerText;
+
+
+        const cardInfo = { headTitle, headPara, details, status: "Interview", notes };
+
+        rejectedList = rejectedList.filter(item => item.headTitle !== cardInfo.headTitle);
+
+        const jobExist = interviewList.find(item => item.headTitle === cardInfo.headTitle);
+
+        const status1 = parentNode.querySelector('.status');
+        status1.innerText = 'Interview';
+
+        status1.classList.remove("bg-red-100", "border-red-500", "text-red-500");
+        status1.classList.add("bg-green-100", "border-green-300", "text-green-400")
+        if (!jobExist) {
+            interviewList.push(cardInfo);
+        };
+        calculateCount();
+        if (interviewBtn.classList.contains('bg-black')) {
+            renderInterview();
+        }
+
     }
-    
-    interviewList.find(item=>item.headTitle== cardInfo)
+    if (event.target.classList.contains('SayRejected')) {
+        const parentNode = event.target.closest('.card');
+        const headTitle = parentNode.querySelector('.headTitle').innerText;
+        const headPara = parentNode.querySelector('.headPara').innerText;
+        const details = parentNode.querySelector('.details').innerText;
+        const status = parentNode.querySelector('.status').innerText;
+        const notes = parentNode.querySelector('.notes').innerText;
+        const cardInfo = { headTitle, headPara, details, status: "Rejected", notes };
+
+        interviewList = interviewList.filter(item => item.headTitle != headTitle);
+        const jobNo = rejectedList.find(item => item.headTitle === cardInfo.headTitle);
+
+        const status1 = parentNode.querySelector('.status');
+        status1.innerText = 'Rejected';
+        status1.classList.add("bg-red-100", "border-red-500", "text-red-500");
+        status1.classList.remove("bg-green-100", "border-green-300", "text-green-400")
+        if (!jobNo) {
+            rejectedList.push(cardInfo);
+        }
+        calculateCount();
+        if (rejectedBtn.classList.contains('bg-black')) {
+            renderRejected();
+        }
+
+    }
 
 })
+
+// render
+function renderInterview() {
+    filterSection.innerHTML = "";
+    for (let inter of interviewList) {
+        let div = document.createElement('div');
+        div.className = 'card px-3 py-2 m-2 bg-white shadow space-y-4 flex justify-between rounded-xl'
+        div.innerHTML = `
+         <div class="space-y-3.5 ">
+     <div>
+         <h1 class="text-xl font-bold headTitle">${inter.headTitle}</h1>
+         <p class="headPara">${inter.headPara}</p>
+     </div>
+     <p class="details">${inter.details}</p>
+     <p class=" status inline-block bg-green-100 px-3 py-1 rounded border border-green-500 text-green-500">${inter.status}</p>
+     <p class="notes">${inter.notes}</p>
+     <div class="flex gap-1">
+         <button
+             class="callInterview  btn p-2 m-2 border rounded border-green-300 text-green-400">Interview</button>
+         <button
+             class=" SayRejected btn border rounded border-red-600 text-red-500 p-2 m-2">Rejected</button>
+     </div>
+ </div>
+ <!-- second part -->
+ <div>
+     <button class=" btn-delete w-10 h-10 rounded-full border flex items-center justify-center"><i
+             class="fa-regular fa-trash-can "></i></button>
+ </div>
+        `
+        filterSection.appendChild(div);
+    }
+}
+
+function renderRejected() {
+    filterSection.innerHTML = "";
+    for (let reject of rejectedList) {
+        let div = document.createElement('div');
+        div.className = 'card px-3 py-2 m-2 bg-white shadow space-y-4 flex justify-between rounded-xl'
+        div.innerHTML = `
+         <div class="space-y-3.5 ">
+     <div>
+         <h1 class="text-xl font-bold headTitle">${reject.headTitle}</h1>
+         <p class="headPara">${reject.headPara}</p>
+     </div>
+     <p class="details">${reject.details}</p>
+     <p class=" status inline-block bg-red-100 px-3 py-1 rounded border border-red-500 text-red-500">${reject.status}</p>
+     <p class="notes">${reject.notes}</p>
+     <div class="flex gap-1">
+         <button
+             class="callInterview  btn p-2 m-2 border rounded border-green-300 text-green-400">Interview</button>
+         <button
+             class=" SayRejected btn border rounded border-red-600 text-red-500 p-2 m-2">Rejected</button>
+     </div>
+ </div>
+ <!-- second part -->
+ <div>
+     <button class=" btn-delete w-10 h-10 rounded-full border flex items-center justify-center"><i
+             class="fa-regular fa-trash-can "></i></button>
+ </div>
+        `
+        filterSection.appendChild(div);
+    }
+
+
+}
+
 
 
 //btn toggle
@@ -94,16 +199,35 @@ const filtered = document.getElementById('filtered');
 allBtn.addEventListener('click', function () {
     cardsSection.classList.remove('hidden');
     filtered.classList.add('hidden');
+    filterSection.classList.add('hidden');
 })
 
 interviewBtn.addEventListener('click', function () {
     cardsSection.classList.add('hidden');
-    filtered.classList.remove('hidden');
+
+    if (interviewList.length === 0) {
+        filtered.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    } else {
+        filtered.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderInterview();
+    }
 })
 
 rejectedBtn.addEventListener('click', function () {
     cardsSection.classList.add('hidden');
-    filtered.classList.remove('hidden');
+
+    if (rejectedList.length === 0) {
+        filtered.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    } else {
+        filtered.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderRejected();
+    }
+
+
 })
 
 
